4d4e9e29baef9931d77985c9a72f3ba618462cd2
Iryna Movchan <movchaniryna@ukr.net>
2022-08-25 00:36:47 +0300

    Добавляет задание 1

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const categoriesEl = document.querySelector("#categories");
+console.log("Number of categories:", categoriesEl.children.length);
+
+const categoryEl = categoriesEl.querySelectorAll(".item");
+
+categoryEl.forEach((item) => {
+  console.log("");
+  console.log("Category:", item.firstElementChild.textContent);
+  console.log("Elements:", item.lastElementChild.children.length);
+});
