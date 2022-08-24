625f5da7fb466e974290e09d7fadfcb40f3e0647
Iryna Movchan <movchaniryna@ukr.net>
2022-08-25 00:45:25 +0300

    Добавляет задание 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientArr = ingredients.map((ingredient) => {
+  const ingredientEl = document.createElement("li");
+  ingredientEl.textContent = ingredient;
+  ingredientEl.classList.add("item");
+  return ingredientEl;
+});
+
+const ingredientsEl = document.querySelector("#ingredients");
+ingredientsEl.append(...ingredientArr);
+
+console.log(ingredientsEl);
