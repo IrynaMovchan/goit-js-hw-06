0f66c41cc17353a624b2bc20ea6adcd81675da84
Iryna Movchan <movchaniryna@ukr.net>
2022-08-25 22:34:32 +0300

    Добавляет задание 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", onLoginFormSubmit);
+
+const formElements = loginForm.elements;
+
+function onLoginFormSubmit(event) {
+  event.preventDefault();
+
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (formElements.email.value === "" || formElements.password.value === "") {
+    return alert("You need to fill in all fields!");
+  }
+
+  console.log({ email, password });
+  loginForm.reset();
+}
